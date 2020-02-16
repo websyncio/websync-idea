270cb661add9c942cf3458dea8aa2669d04e20b6
Vitalii Balitckii <Vitalii_Balitckii@epam.com>
2020-02-16 20:15:36 +0300

    implemented searching classes extended WebPage

--- src/main/java/com/epam/sha/intellij/websync/utils/FileParser.java
+++ src/main/java/com/epam/sha/intellij/websync/utils/FileParser.java
@@ -1,9 +1,25 @@
 package com.epam.sha.intellij.websync.utils;
 
+import com.intellij.execution.filters.TextConsoleBuilderFactory;
+import com.intellij.execution.ui.ConsoleView;
+import com.intellij.execution.ui.ConsoleViewContentType;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.wm.ToolWindow;
+import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.psi.JavaPsiFacade;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.search.GlobalSearchScope;
+import com.intellij.psi.search.PsiShortNamesCache;
+import com.intellij.psi.search.searches.AllClassesSearch;
+import com.intellij.ui.content.Content;
+import com.intellij.ui.content.MessageView;
+import com.jediterm.terminal.ui.TerminalPanel;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FileParser {
     public void parse(List<String> lines) {
@@ -11,7 +27,7 @@ public class FileParser {
             System.out.println("File is empty.");
             return;
         }
-        Project currentProject = ProjectManager.getInstance().getOpenProjects()[0];
+
         switch (lines.get(0)) {
             case "a":
                 System.out.println("Command 'a'");
@@ -21,9 +37,29 @@ public class FileParser {
                 break;
             case "print":
                 System.out.println("Print");
+                printClasses();
                 break;
             default:
                 System.out.println(String.format("Any command '%s'", lines.get(0)));
         }
+    }
+
+    private void printClasses() {
+        Project project = ProjectManager.getInstance().getOpenProjects()[0];
+//        String[] classes = PsiShortNamesCache.getInstance(project).getAllClassNames();
+
+        JavaPsiFacade javaPsiFacade = JavaPsiFacade.getInstance(project);
+        GlobalSearchScope projectScope = GlobalSearchScope.projectScope(project);
+
+        ApplicationManager.getApplication().runReadAction(() -> {
+            List<PsiClass> classes = AllClassesSearch.search(projectScope, project).findAll().stream()
+                    .filter(c -> Arrays.asList(c.getSupers()).stream()
+                            .filter(s -> s.getQualifiedName().equals("com.epam.jdi.light.elements.composite.WebPage"))
+                            .count() > 0)
+                    .collect(Collectors.toList());
+
+            System.out.println("Classes:");
+            classes.forEach(c -> System.out.println("*" + c.getQualifiedName()));
+        });
     }
 }
