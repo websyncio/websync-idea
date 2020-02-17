c3ac9d7a2290b79eab00bc103a8d72b600ac0dd8
Irina_Gordienko <Irina_Gordienko@epam.com>
2020-02-17 17:27:47 +0300

    added two classes

---
+++ src/main/java/com/epam/sha/intellij/websync/jdi/JdiAttributes.java
@@ -0,0 +1,32 @@
+package com.epam.sha.intellij.websync.jdi;
+
+public class JdiAttributes {
+    public static final String JDI_BY_TEXT = "com.epam.jdi.light.elements.pageobjects.annotations.locators.ByText";
+    public static final String JDI_CSS = "com.epam.jdi.light.elements.pageobjects.annotations.locators.Css";
+    public static final String JDI_JDROPDOWN = "com.epam.jdi.light.elements.pageobjects.annotations.locators.JDropdown";
+    public static final String JDI_JTABLE = "com.epam.jdi.light.elements.pageobjects.annotations.locators.JTable";
+    public static final String JDI_JMENU = "com.epam.jdi.light.elements.pageobjects.annotations.locators.JMenu";
+    public static final String JDI_JSITE = "com.epam.jdi.light.elements.pageobjects.annotations.JSite";
+    public static final String JDI_WITH_TEXT = "com.epam.jdi.light.elements.pageobjects.annotations.locators.WithText";
+    public static final String JDI_XPATH = "com.epam.jdi.light.elements.pageobjects.annotations.locators.XPath";
+    public static final String JDI_CLICK_AREA = "com.epam.jdi.light.elements.pageobjects.annotations.ClickArea";
+    public static final String JDI_FIND_BY = "com.epam.jdi.light.elements.pageobjects.annotations.FindBy";
+    public static final String JDI_FIND_BYS = "com.epam.jdi.light.elements.pageobjects.annotations.FindBys";
+    public static final String JDI_FRAME = "com.epam.jdi.light.elements.pageobjects.annotations.Frame";
+    public static final String JDI_GET_ANY = "com.epam.jdi.light.elements.pageobjects.annotations.GetAny";
+    public static final String JDI_GET_SHOW_IN_VIEW = "com.epam.jdi.light.elements.pageobjects.annotations.GetShowInView";
+    public static final String JDI_GET_TEXT_AS = "com.epam.jdi.light.elements.pageobjects.annotations.GetTextAs";
+    public static final String JDI_GET_VISIBLE = "com.epam.jdi.light.elements.pageobjects.annotations.GetVisible";
+    public static final String JDI_GET_VISIBLE_ENABLED = "com.epam.jdi.light.elements.pageobjects.annotations.GetVisibleEnabled";
+    public static final String JDI_MANDATORY = "com.epam.jdi.light.elements.pageobjects.annotations.Mandatory";
+    public static final String JDI_NAME = "com.epam.jdi.light.elements.pageobjects.annotations.Name";
+    public static final String JDI_NO_CACHE = "com.epam.jdi.light.elements.pageobjects.annotations.NoCache";
+    public static final String JDI_NO_WAIT = "com.epam.jdi.light.elements.pageobjects.annotations.NoWait";
+    public static final String JDI_ROOT = "com.epam.jdi.light.elements.pageobjects.annotations.Root";
+    public static final String JDI_PAGE_NAME = "com.epam.jdi.light.elements.pageobjects.annotations.PageName";
+    public static final String JDI_SET_TEXT_AS = "com.epam.jdi.light.elements.pageobjects.annotations.SetTextAs";
+    public static final String JDI_TITLE = "com.epam.jdi.light.elements.pageobjects.annotations.Title";
+    public static final String JDI_URL = "com.epam.jdi.light.elements.pageobjects.annotations.Url";
+    public static final String JDI_VISUAL_CHECK = "com.epam.jdi.light.elements.pageobjects.annotations.VisualCheck";
+    public static final String JDI_WAIT_TIMEOUT = "com.epam.jdi.light.elements.pageobjects.annotations.WaitTimeout";
+}
--- src/main/java/com/epam/sha/intellij/websync/jdi/JdiLocators.java
+++
@@ -1,7 +0,0 @@
-package com.epam.sha.intellij.websync.jdi;
-
-/**
- * Created by Irina_Gordienko on 17-Feb-20
- */
-public class JdiLocators {
-}
