c7eee3d539732a8144a3a36b9fb361701b4e907e
Vitalii Balitckii <Vitalii_Balitckii@epam.com>
2020-02-28 17:21:06 +0300

    replaced supported-frameworks

---
+++ supported-frameworks/jdi-x.x.x/src/main/java/org/websync/jdi/InheritedPageObject.java
@@ -0,0 +1,25 @@
+package org.websync.supportedframeworks.jdi;
+
+
+import com.epam.jdi.light.elements.complex.*;
+import com.epam.jdi.light.elements.pageobjects.annotations.FindBy;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.ByText;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.Css;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.XPath;
+
+public class InheritedPageObject extends BasePageObject {
+    @Css(".testCss")
+    public Checklist initializedChecklist;
+    @XPath("//testXpath")
+    public Combobox initializedCombobox;
+    @ByText("test")
+    public DataList initializedDataList;
+    @Css(".testCss")
+    public JList initializedJList;
+    @FindBy(xpath = "//test")
+    public Selector initializedSelector;
+    @Css(".testCss")
+    public SelectWrapper initializedSelectWrapper;
+    @XPath("//testXpath")
+    public WebList initializedWebList;
+}
---
+++ supported-frameworks/jdi-x.x.x/src/main/java/org/websync/jdi/InheritedPageObject2.java
@@ -0,0 +1,25 @@
+package org.websync.supportedframeworks.jdi;
+
+import com.epam.jdi.light.elements.pageobjects.annotations.FindBy;
+import com.epam.jdi.light.elements.pageobjects.annotations.locators.*;
+import com.epam.jdi.light.ui.html.elements.common.*;
+
+public class InheritedPageObject2 extends InheritedPageObject {
+
+    @Css(".testCss")
+    public Button initializedButton;
+    @XPath("//testXpath")
+    public Checkbox initializedCheckbox;
+    @ByText("test")
+    public Image initializedImage;
+    @UI("test")
+    public Link initializedLink;
+    @WithText("test")
+    public TextField initializedTextField;
+    @FindBy(id = "test")
+    public Icon initializedIcon;
+    @UI("#custom-element")
+    public CustomElement initializedCustomElement;
+    @UI("#custom-base-element")
+    public CustomBaseElement initializedCustomBaseElement;
+}
