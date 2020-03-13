f38ad3bc4e756ccf49880943a6812b93a2119560
Irina_Gordienko <Irina_Gordienko@epam.com>
2020-03-13 13:01:43 +0300

    created BrowserConnection class

--- plugin/src/main/java/org/websync/InitializationComponent.java
+++ plugin/src/main/java/org/websync/InitializationComponent.java
@@ -1,21 +1,29 @@
 package org.websync;
 
-import org.websync.utils.DebugFileWatcher;
-import org.websync.utils.FileParser;
 import com.intellij.openapi.components.BaseComponent;
 import org.jetbrains.annotations.NotNull;
+import org.websync.utils.DebugFileWatcher;
+import org.websync.utils.FileParser;
 
 import java.io.File;
+import java.net.ServerSocket;
 
 public class InitializationComponent implements BaseComponent {
 
     DebugFileWatcher debugFileWatcher = new DebugFileWatcher(new File("c:/temp/text.txt"), new FileParser());
+    BrowserConnection browserConnection = new BrowserConnection("localhost", 1804);
+
+    private ServerSocket serverSocket;
+    private Thread listenerThread;
+
+    public InitializationComponent() {
+    }
 
     public void initComponent() {
         System.out.println("Initializing...");
 
         debugFileWatcher.start();
-
+        browserConnection.initConnection();
         System.out.println("Initialized.");
     }
 
@@ -23,7 +31,7 @@ public class InitializationComponent implements BaseComponent {
         System.out.println("Disposing...");
 
         debugFileWatcher.stop();
-
+        browserConnection.disposeConnection();
         System.out.println("Disposed.");
     }
 
