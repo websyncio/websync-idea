e3cc4a04d9f63e6c80c1cf78bba432acd1d7dee3
Irina <31936769+igordien@users.noreply.github.com>
2020-03-16 17:41:06 +0300

    created BrowserConnection class (#70)
    * created BrowserConnection class
    
    * fixed websocket server initialization
    
    Co-authored-by: Irina_Gordienko <Irina_Gordienko@epam.com>
    Co-authored-by: Aleksey Volkov <Aleksey_Volkov@epam.com>

---
+++ plugin/src/main/java/org/websync/BrowserConnection.java
@@ -0,0 +1,48 @@
+package org.websync;
+
+import com.intellij.openapi.diagnostic.Logger;
+import org.java_websocket.server.WebSocketServer;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+
+
+public class BrowserConnection {
+    private static final Logger log = Logger.getInstance(BrowserConnection.class);
+    private ServerSocket serverSocket;
+    private WebSocketServer server;
+    private Thread listenerThread;
+    private String host;
+    private Integer port;
+//    private String connectionName;
+
+    public BrowserConnection(String host, Integer port) {
+        this.host = host;
+        this.port = port;
+//        this.connectionName = connectionName;
+    }
+
+    public void initConnection() {
+        server = new Server(port);
+        server.start();
+        log.info("Listening " + port);
+    }
+
+    public void disposeConnection() {
+        try {
+            if (listenerThread != null) {
+                listenerThread.interrupt();
+            }
+            server.stop();
+        } catch (InterruptedException | IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @NotNull
+    public String getComponentName() {
+        return "BrowserConnection";
+    }
+}
