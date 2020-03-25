f79e59ab8b9f4d30030eb8d88f39aca1888864ac
Vitalii Balitckii <Vitalii_Balitckii@epam.com>
2020-03-25 18:31:09 +0300

    added command CMD_GET_PAGEOBJECTS

--- plugin/src/main/java/org/websync/browserConnection/CommandHandler.java
+++ plugin/src/main/java/org/websync/browserConnection/CommandHandler.java
@@ -7,6 +7,8 @@ import org.websync.websession.WebSessionPovider;
 
 public class CommandHandler {
     final String CMD_GIVE_ME_PAGEOBECT = "give me page object";
+    final String CMD_GET_PAGEOBJECTS = "get page objects";
+
     WebSessionPovider webSessionPovider;
     WebSessionSerializer webSessionSerializer;
 
@@ -18,6 +20,7 @@ public class CommandHandler {
     public String handle(String command) {
         switch (command) {
             case CMD_GIVE_ME_PAGEOBECT:
+            case CMD_GET_PAGEOBJECTS:
                 return new GetWebSessionCommand(webSessionPovider, webSessionSerializer).execute();
         }
         return null;
