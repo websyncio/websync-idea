d5afa35ccf7c39896369b22f1f8686b7001c0504
Vitalii Balitckii <Vitalii_Balitckii@epam.com>
2020-04-07 13:32:53 +0300

    dfs

--- plugin/src/main/java/org/websync/debugger/commands/CommandTestSerializer.java
+++ plugin/src/main/java/org/websync/debugger/commands/CommandTestSerializer.java
@@ -6,7 +6,13 @@ import org.junit.Test;
 import org.websync.WebSyncService;
 import org.websync.debugger.testengine.TestEngine;
 import org.websync.websession.WebSessionProvider;
+import org.websync.websession.models.ComponentInstance;
+import org.websync.websession.models.ComponentType;
 import org.websync.websession.models.WebSession;
+import org.websync.websession.psimodels.PsiComponentInstance;
+
+import java.util.List;
+import java.util.Map;
 
 public class CommandTestSerializer {
     public static void run() {
@@ -19,11 +25,19 @@ public class CommandTestSerializer {
     public static void testDeserializing() {
         WebSyncService webSyncService = ServiceManager.getService(WebSyncService.class);
 
-        WebSessionProvider provider = webSyncService.getProvider();
-        String json = webSyncService.getSerializer().serialize(provider.getWebSession(
-                provider.getProjects().get(0)));
-
+//        WebSessionProvider provider = webSyncService.getProvider();
+//        String json = webSyncService.getSerializer().serialize(provider.getWebSession(
+//                provider.getProjects().get(0)));
+//
         WebSession websession = webSyncService.getSerializer().deserialize(json);
+
+        WebSessionProvider provider = webSyncService.getProvider();
+        WebSession webSession0 = provider.getWebSessions(true).get(0);
+        WebSession webSession1 = provider.getWebSessions(true).get(0);
+        ComponentType componentType = webSession0.getComponentTypes().values().stream().findAny().get();
+        ComponentInstance componentInstance = componentType.getComponentInstances().stream().findAny().get();
+        PsiComponentInstance psiComponentInstance = (PsiComponentInstance) componentInstance;
+        psiComponentInstance.synchronize(psiComponentInstance);
     }
 }
 
