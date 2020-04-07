5af62608d95d6601d492bb1d86a8d3b2ca520c21
Irina_Gordienko <Irina_Gordienko@epam.com>
2020-04-07 19:03:45 +0300

    added ButtonAction

---
+++ plugin/src/main/java/com/epam/sha/intellij/locatorupdater/ButtonAction.java
@@ -0,0 +1,17 @@
+package com.epam.sha.intellij.locatorupdater;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class ButtonAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+
+    @Override
+    public void update(@NotNull AnActionEvent e) {
+
+    }
+}
