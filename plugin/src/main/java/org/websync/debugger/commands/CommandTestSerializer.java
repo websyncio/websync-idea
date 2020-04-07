1d2b529c58756ca519ad99064dc6e013d406ab7b
Vitalii Balitckii <Vitalii_Balitckii@epam.com>
2020-04-07 13:33:48 +0300

    fsdfs

--- plugin/src/main/java/org/websync/debugger/commands/CommandTestSerializer.java
+++ plugin/src/main/java/org/websync/debugger/commands/CommandTestSerializer.java
@@ -25,13 +25,12 @@ public class CommandTestSerializer {
     public static void testDeserializing() {
         WebSyncService webSyncService = ServiceManager.getService(WebSyncService.class);
 
-//        WebSessionProvider provider = webSyncService.getProvider();
-//        String json = webSyncService.getSerializer().serialize(provider.getWebSession(
-//                provider.getProjects().get(0)));
-//
+        WebSessionProvider provider = webSyncService.getProvider();
+        String json = webSyncService.getSerializer().serialize(provider.getWebSession(
+                provider.getProjects().get(0)));
+
         WebSession websession = webSyncService.getSerializer().deserialize(json);
 
-        WebSessionProvider provider = webSyncService.getProvider();
         WebSession webSession0 = provider.getWebSessions(true).get(0);
         WebSession webSession1 = provider.getWebSessions(true).get(0);
         ComponentType componentType = webSession0.getComponentTypes().values().stream().findAny().get();
