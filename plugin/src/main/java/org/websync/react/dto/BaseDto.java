b453ebb0a3ad1de7f3556219b920b9eb28935b5e
Alexey_Chmutov <Alexey_Chmutov@epam.com>
2020-04-10 02:12:23 +0300

    default constructors added

--- plugin/src/main/java/org/websync/react/dto/BaseDto.java
+++ plugin/src/main/java/org/websync/react/dto/BaseDto.java
@@ -3,6 +3,9 @@ package org.websync.react.dto;
 public class BaseDto {
     public String id;
 
+    public BaseDto() {
+    }
+
     public BaseDto(String id) {
         this.id = id;
     }
--- plugin/src/main/java/org/websync/websocket/WebSessionSerializer.java
+++ plugin/src/main/java/org/websync/websocket/WebSessionSerializer.java
@@ -1,11 +1,25 @@
 package org.websync.websocket;
 
+import org.websync.react.ReactSerializer;
+import org.websync.react.dto.ComponentTypeDto;
+import org.websync.react.dto.PageTypeDto;
+import org.websync.react.dto.WebsiteDto;
 import org.websync.websession.models.WebSession;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public interface WebSessionSerializer {
-    String serialize(List<WebSession> webSessions);
+
     String serialize(WebSession webSession);
-    WebSession deserialize(String json);
+
+    ReactSerializer.ReactDataPayload deserialize(String json) throws IOException;
+
+    class ReactDataPayload {
+        public List<WebsiteDto> websites = new ArrayList<>();
+        public List<PageTypeDto> pages = new ArrayList<>();
+        public List<ComponentTypeDto> components = new ArrayList<>();
+    }
+
 }
