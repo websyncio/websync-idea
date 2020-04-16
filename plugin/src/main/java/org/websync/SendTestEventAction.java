a8a37a9b90689a67fa2dad07c14baf90f255e4c7
tsvetaevAlex <56589282+tsvetaevAlex@users.noreply.github.com>
2020-04-16 18:52:46 +0300

    revert SendTestEventAction

--- plugin/src/main/java/org/websync/SendTestEventAction.java
+++ plugin/src/main/java/org/websync/SendTestEventAction.java
@@ -1,29 +1,49 @@
-package org.websync.jdi;
+package org.websync;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.fileChooser.FileChooser;
+import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.vfs.VirtualFile;
+import org.java_websocket.client.WebSocketClient;
+import org.java_websocket.handshake.ServerHandshake;
+import org.jetbrains.annotations.NotNull;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-import com.epam.jdi.light.elements.common.Label;
-import com.epam.jdi.light.elements.composite.Section;
-import com.epam.jdi.light.elements.pageobjects.annotations.locators.*;
-
-public class AttributesTest extends Section {
-    @ByText(value = "value")
-    Label byText;
-    @Css(value = "value")
-    Label css1;
-    @JDropdown(root = "root1", value = "value1", expand = "expand1", list = "list1", autoclose = true)
-    Label jDropdown;
-    @JMenu(value = {"value1", "value2"}, group = "group1")
-    Label jMenu;
-    @JTable(
-            root = "root1", header = {"header1", "header2"}, headers = "headers1", filter = "filter1", row = "row1",
-            column = "column1", cell = "cell1", allCells = "allCells1", rowHeader = "rowHeader1",
-            fromCellToRow = "fromCellToRow1", size = 1, count = 1, firstColumnIndex = 1, columnsMapping = {1,2,3})
-    Label jTable;
-    @UI(value = "value1", group = "group1")
-    Label ui(Label = "value1");
-    @UI.List({@UI(value = "value1", group = "group1"), @UI(value = "value1", group = "group1")})
-    Label uiList;
-    @WithText(value = "value1")
-    Label withText;
-    @XPath(value = "value1")
-    Label xpath;
+public class SendTestEventAction extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+        final FileChooserDescriptor desc = new FileChooserDescriptor(true, false, false, false, false, false);
+        VirtualFile f = FileChooser.chooseFile(desc, e.getProject(), null);
+        if(f == null) {
+            System.out.println("No command file was chosen!");
+            return;
+        }
+        try {
+            String message = new String(f.contentsToByteArray());
+            WebSocketClient client = createClient();
+            client.connectBlocking();
+            client.send(message);
+            client.close();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    private WebSocketClient createClient() throws URISyntaxException {
+        URI uri = new URI("http://127.0.0.1:" + WebSyncService.getPortFromConfig());
+        return new WebSocketClient(uri) {
+            @Override
+            public void onOpen(ServerHandshake handshakedata) {
+            }
+            @Override
+            public void onMessage(String message) {
+            }
+            @Override
+            public void onClose(int code, String reason, boolean remote) {
+            }
+            @Override
+            public void onError(Exception ex) {
+            }
+        };
+    }
 }
