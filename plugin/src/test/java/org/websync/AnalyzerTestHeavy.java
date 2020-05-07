43ce4e00169dadc23ff18bc6fedb3195a6bdb172
tsvetaevAlex <56589282+tsvetaevAlex@users.noreply.github.com>
2020-05-07 15:31:20 +0300

    remove private  valiable: private String srcPath ileDocumentManagerImpl impl reason: unused even in class where declared, hard code of local path 2 import line which become unused because of variables removement

--- plugin/src/test/java/org/websync/AnalyzerTestHeavy.java
+++ plugin/src/test/java/org/websync/AnalyzerTestHeavy.java
@@ -2,9 +2,11 @@ package org.websync;
 
 import com.intellij.ide.projectWizard.ImportFromSourcesTestCase;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.fileEditor.impl.FileDocumentManagerImpl;
 import com.intellij.openapi.project.ProjectManager;
+import com.intellij.openapi.roots.ProjectRootManager;
+import com.intellij.openapi.vfs.VirtualFile;
 import org.jdom.JDOMException;
-import org.junit.Assert;
 
 import java.io.File;
 import java.io.IOException;
@@ -13,7 +15,7 @@ import java.nio.file.Paths;
 
 public class AnalyzerTestHeavy extends ImportFromSourcesTestCase {
 
-    private File file = new File(srcPath);
+    File file = new File(srcPath);
 
     public void test0() {
         try {
@@ -26,14 +28,13 @@ public class AnalyzerTestHeavy extends ImportFromSourcesTestCase {
 
         importFromSources(file);
         this.importFromSources(new File(srcPath));
-
-        //TODO:include reasonable assertion
-        /*
-        //temporary assert to hide codacy warning
-        //reason:
-        //Since: PMD 2.0
-        //JUnit tests should include at least one assertion.
-        */
-        Assert.assertTrue(true);
+        try {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
