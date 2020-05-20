b90cd1c1b80915df2b7607a4c978c952041c972b
Irina_Gordienko <Irina_Gordienko@epam.com>
2020-05-20 14:37:58 +0300

    small additions

--- plugin/src/main/java/org/websync/websession/models/Website.java
+++ plugin/src/main/java/org/websync/websession/models/Website.java
@@ -1,4 +1,13 @@
 package org.websync.websession.models;
 
+import com.intellij.psi.PsiClass;
+
+import java.util.List;
+
 public interface Website {
+    String getName(PsiClass psiClass);
+
+    String getUrl(PsiClass psiClass);
+
+    List<PageType> getPageTypes();
 }
--- plugin/src/main/java/org/websync/websession/psimodels/PsiWebsite.java
+++ plugin/src/main/java/org/websync/websession/psimodels/PsiWebsite.java
@@ -1,7 +1,11 @@
 package org.websync.websession.psimodels;
 
+import com.epam.jdi.light.elements.pageobjects.annotations.JSite;
 import com.intellij.psi.PsiClass;
+import org.websync.websession.models.PageType;
 import org.websync.websession.models.Website;
+
+import java.util.List;
 
 public class PsiWebsite extends PsiNamedTypeWrapper<Website> implements Website {
     public PsiWebsite(PsiClass psiClass) {
@@ -11,5 +15,23 @@ public class PsiWebsite extends PsiNamedTypeWrapper<Website> implements Website 
     @Override
     public void fill() {
         super.fill();
+    }
+
+    @Override
+    public String getName(PsiClass psiClass) {
+        return psiClass.getQualifiedName();
+    }
+
+    @Override
+    public String getUrl(PsiClass psiClass) {
+//        JSite column = psiClass.getAnnotation(JSite.class);
+//        if (column != null)
+//            System.out.println(column.columnName());
+        return null;
+    }
+
+    @Override
+    public List<PageType> getPageTypes() {
+        return null;
     }
 }
